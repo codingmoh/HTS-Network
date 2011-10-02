ff91467214cefc64e0fecc9af68e874a6ff6c549
abrajoe <fiftycent1@sms.at>
2011-10-02 22:18:10 +0200

    implemented a connection function, parsing a string to an ip adress

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,6 +1,13 @@
 #include "client.h"
 
-void Client::connect_to_target(sockaddr * target_addr)
+Client::Client(NetworkAddressType addresstype, NetworkProtocolType protocoltype, int port ):Networkbase(addresstype, protocoltype, port)
 {
-  connect(this->socket_descriptor, target_addr, sizeof(target_addr) );
+  
+}
+
+void Client::connect_to_target(std::string ip)
+{
+  sockaddr_in addr;
+  inet_aton (ip.c_str(), &this->addr.sin_addr);
+  connect(this->socket_descriptor, (sockaddr *) &this->addr, sizeof(this->addr) ); 
 }
