6f87898ccd0c4e855c3d87e87474cc9522f17f77
abrajoe <fiftycent1@sms.at>
2011-10-02 22:17:33 +0200

    basic entry point for starting the server

--- src/server/server_main.cpp
+++ src/server/server_main.cpp
@@ -1,5 +1,8 @@
+#include <server.h>
 
 int main()
 {
-  
+    Server server(Server::IPv4, Server::TCP, 9000);
+    std::cout<<"Server started, waiting for incoming clients"<<std::endl;
+    server.waitforincome();
 }
