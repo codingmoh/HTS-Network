8dfab5b3757c1a2b0ea751561a391e69a486deb1
abrajoe <fiftycent1@sms.at>
2011-10-02 06:23:59 +0200

    added basic implementation for serialization

--- include/message.h
+++ include/message.h
@@ -1,6 +1,40 @@
-template <class T>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/export.hpp>
+#ifndef MESSAGE_H
+#define MESSAGE_H
 class Message
-{
+{  
+  protected:
+    enum Messagetype
+    {
+      mMail
+    };
+    
   public:
-    T message;
+    Message(Message::Messagetype type)
+    {
+      this->_msg_type = type;
+    };
+    Message(){};
+    
+    Messagetype getmessagetype()
+    {
+      return _msg_type;
+    };
+    virtual ~Message()
+    {
+      
+    };
+    
+  private:  
+      Messagetype _msg_type;
+      friend class boost::serialization::access;   
+      template<class Archive>
+      void serialize(Archive & ar, const unsigned int version)
+      {
+	ar & _msg_type;
+      }
 };
+#endif
+
