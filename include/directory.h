6f72a3e5cf1a93794f37218863e5c7787e479f0f
abrajoe <fiftycent1@sms.at>
2011-10-08 15:03:39 +0200

    Revert "m"
    This reverts commit c17abc9de4c00f9423d6a809e1539161ba6353f7.
    
    Conflicts:
    
    	include/directory.h

--- include/directory.h
+++ include/directory.h
@@ -56,8 +56,12 @@ private:
 	bool existsmaildirOtherwiseCreate(std::string, std::string);
 	std::string getfreemessagenumber(std::string);
 <<<<<<< HEAD
+<<<<<<< HEAD
 	bool create(std::string);
 >>>>>>> c17abc9de4c00f9423d6a809e1539161ba6353f7
+=======
+	void create(std::string);
+>>>>>>> parent of c17abc9... m
 =======
 	void create(std::string);
 >>>>>>> parent of c17abc9... m
