2e298da6065230bc471051ff72c3e5b99ed1d3a8
armin2006n <armin_juju@hotmail.com>
2011-10-10 08:43:29 -0700

    executecommand um messageexception erweitert

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,44 +1,55 @@
 #include <string.h>
 #include "client.h"
 #include <listmessage.h>
+#include <messageexception.h>
 
-Client::Client(NetworkAddressType addr_esstype, 
-	       NetworkProtocolType protocoltype, 
-	       int port, std::string username):
-	       Networkbase(addr_esstype, protocoltype, port)
+Client::Client(NetworkAddressType addr_esstype,
+    NetworkProtocolType protocoltype, int port, std::string username) :
+  Networkbase(addr_esstype, protocoltype, port)
 {
- 
+
 }
 
-bool Client::connect_to_target(std::string ip)
+bool
+Client::connect_to_target(std::string ip)
 {
   sockaddr_in addr_;
-  inet_aton (ip.c_str(), &this->addr_.sin_addr);
- if(connect(this->socket_descriptor_, (sockaddr *) &this->addr_, sizeof(this->addr_)) == 0)
-   return true;
- else
-   return false;
+  inet_aton(ip.c_str(), &this->addr_.sin_addr);
+  if (connect(this->socket_descriptor_, (sockaddr *) &this->addr_,
+      sizeof(this->addr_)) == 0)
+    return true;
+  else
+    return false;
 }
 
-void Client::sendmessage(Message *& message)
+void
+Client::sendmessage(Message *& message)
 {
-   Serializer::sendmessage(this->socket_descriptor_, message);
+  Serializer::sendmessage(this->socket_descriptor_, message);
 }
-void Client::waitresponse()
+void
+Client::waitresponse()
 {
   Serializer::receivemessage(this->socket_descriptor_, 2048);
 }
 
-void Client::executecommand(Message*& message)
+void
+Client::executecommand(Message*& message)
 {
-  
-  std::cout<<message->getmessagetype()<<std::endl;
-  if(message->getmessagetype()==Message::mList)
-  {
-    std::vector<ListMessageElement> elem = dynamic_cast<Listmessage*>(message)->GetElements();
-    for(int i = 0; i< elem.size(); i++)
+
+  std::cout << message->getmessagetype() << std::endl;
+  if (message->getmessagetype() == Message::mList)
     {
-      std::cout<<elem[i].number_<<":"<<elem[i].subject_<<std::endl;
+      std::vector<ListMessageElement> elem =
+          dynamic_cast<Listmessage*> (message)->GetElements();
+      for (int i = 0; i < elem.size(); i++)
+        {
+          std::cout << elem[i].number_ << ":" << elem[i].subject_ << std::endl;
+        }
     }
-  }
+  else if (message->getmessagetype() == Message::mException)
+    {
+      std::cout << dynamic_cast<MessageException*> (message)->err_ << ": "
+          << dynamic_cast<MessageException*> (message)->msg_ << std::endl;
+    }
 }
