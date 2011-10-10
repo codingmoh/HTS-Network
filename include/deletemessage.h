0db5b3d73927118524439e97bb34c5e003cf3c94
abrajoe <fiftycent1@sms.at>
2011-10-10 20:21:45 +0200

    implemented deletemessages

---
+++ include/deletemessage.h
@@ -0,0 +1,33 @@
+
+#ifndef DELETEMESSAGE_H
+#define DELETEMESSAGE_H
+#include "message.h"
+
+class Deletemessage: public Message
+{
+   friend class boost::serialization::access;
+   template<class Archive>
+   void serialize(Archive & ar, const unsigned int version)
+   {
+      boost::serialization::base_object<Message>(*this);
+      ar & number_;
+   }
+
+   
+
+public:
+   int number_;
+   std::string user_;
+   Deletemessage(int nr, std::string name) :
+         Message(Message::mDelete), number_(nr), user_(name)
+   {
+   }
+
+   Deletemessage(): Message(Message::mMail)
+   {
+
+   }
+};
+
+BOOST_CLASS_EXPORT(Deletemessage);
+#endif // SERVER_H
