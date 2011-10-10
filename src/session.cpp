82310e547fd1da8f181fba6f16625a28f1b0c598
armin2006n <armin_juju@hotmail.com>
2011-10-10 08:42:50 -0700

    try und catch um execute, falls fehler gefangen wird, wird messageexception zurückgeschickt

--- src/session.cpp
+++ src/session.cpp
@@ -1,48 +1,60 @@
 #include <session.h>
 
 
-Session::Session(int socketid, Directory& userdir):userdir_(userdir), socketid_(socketid)
+Session::Session(int socketid, Directory& userdir) :
+  userdir_(userdir), socketid_(socketid)
 {
-  
+
 }
 
-void Session::start()
+void
+Session::start()
 {
   boost::thread sessionthread(&Session::startrecieveing, this);
   sessionthread.join();
 }
 
-void Session::startrecieveing()
+void
+Session::startrecieveing()
 {
   Serializer::receivemessage(this->socketid_, 2048);
-   //executecommand();
+  //executecommand();
   /*if()
-  {
-    //SEND OK
-  }
-  else
-  {
-    //SEND ERR
-  }*/
+   {
+   //SEND OK
+   }
+   else
+   {
+   //SEND ERR
+   }*/
 }
 
-void Session::executecommand(Message *& message)
+void
+Session::executecommand(Message *& message)
 {
- if(message->getmessagetype()==Message::mMail)
- {
-   this->userdir_.savemessage(*dynamic_cast<Mail*>(message));
- }
- else if(message->getmessagetype()==Message::mList)
- {
-   std::cout<<"gothere"<<std::endl;
-   Listmessage * lm = dynamic_cast<Listmessage*>(message);
-   std::cout<<lm->GetUserName()<<std::endl;
-   this->userdir_.getmessages(*lm);
-   Message * msg = lm;
-   std::cout<<lm->GetElements()[0].subject_<<std::endl;
-   Serializer::sendmessage(this->socketid_, msg );// ^^ ...OOP FTW xD
- }
- this->startrecieveing();
+  try
+    {
+      if (message->getmessagetype() == Message::mMail)
+        {
+          this->userdir_.savemessage(*dynamic_cast<Mail*> (message));
+        }
+      else if (message->getmessagetype() == Message::mList)
+        {
+          std::cout << "gothere" << std::endl;
+          Listmessage * lm = dynamic_cast<Listmessage*> (message);
+          std::cout << lm->GetUserName() << std::endl;
+          this->userdir_.getmessages(*lm);
+          Message * msg = lm;
+          std::cout << lm->GetElements()[0].subject_ << std::endl;
+          Serializer::sendmessage(this->socketid_, msg);// ^^ ...OOP FTW xD
+        }
+    }
+  catch (MessageException* e)
+    {
+      Message * msg = e;
+      Serializer::sendmessage(this->socketid_, msg);
+    }
+  this->startrecieveing();
 }
 Session::~Session()
 {
