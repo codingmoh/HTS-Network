b16aa4c351a5d0d4bc3c420815f55a6e91dd455a
abrajoe <fiftycent1@sms.at>
2011-11-05 21:37:38 +0100

    you will ned it for serializing in a seperate file, DON'T include in any file -> BOOST_REGISTER... MACRO turned out to be multiple defined

---
+++ include/EXPORTS.h
@@ -0,0 +1,19 @@
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/export.hpp>
+#include "deletemessage.h"
+#include "listmessage.h"
+#include "messageexception.h"
+#include "standard_messages.h"
+#include "read.h"
+#include "mail.h"
+#include "login.h"
+//#include "listmessageelement.h"
+BOOST_CLASS_EXPORT(Deletemessage);
+BOOST_CLASS_EXPORT(Listmessage);
+BOOST_CLASS_EXPORT(MessageException);
+BOOST_CLASS_EXPORT(standard_messages);
+BOOST_CLASS_EXPORT(Read);
+BOOST_CLASS_EXPORT(Mail);
+BOOST_CLASS_EXPORT(Login);
+//BOOST_CLASS_EXPORT(ListMessageElement);
